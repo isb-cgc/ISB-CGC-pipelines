56ad8db0284fc7d80a80ff125549a6e27d965e44
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2016-05-21 17:11:21 -0700

    Updated paths in the install script and the isb-cgc-pipelines top-level script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp -R ../ISB-CGC-pipelines /usr/local/isb_cgc_pipelines
-ln -s /usr/local/isb_cgc_pipelines/lib/isb-cgc-pipelines /usr/bin/isb-cgc-pipelines
-ln -s /usr/local/isb_cgc_pipelines/lib/scheduler/pipelineRunningJobs /usr/bin/pipelineRunningJobs
-ln -s /usr/local/isb_cgc_pipelines/lib/scheduler/pipelineWaitingJobs /usr/bin/pipelineWaitingJobs
-ln -s /usr/local/isb_cgc_pipelines/lib/scheduler/pipelinePreemptedJobs /usr/bin/pipelinePreemptedJobs
-ln -s /usr/local/isb_cgc_pipelines/lib/scheduler/pipelineDependentJobs /usr/bin/pipelineDependentJobs
-ln -s /usr/local/isb_cgc_pipelines/lib/scheduler/pipelineMonitor /usr/bin/pipelineMonitor
+cp -R ../ISB-CGC-pipelines /usr/local/ISB-CGC-pipelines
+ln -s /usr/local/ISB-CGC-pipelines/lib/isb-cgc-pipelines /usr/bin/isb-cgc-pipelines
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineRunningJobs /usr/bin/pipelineRunningJobs
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineWaitingJobs /usr/bin/pipelineWaitingJobs
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelinePreemptedJobs /usr/bin/pipelinePreemptedJobs
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineDependentJobs /usr/bin/pipelineDependentJobs
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineMonitor /usr/bin/pipelineMonitor
 
 
 
