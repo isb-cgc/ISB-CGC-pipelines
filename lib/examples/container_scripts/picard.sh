8f7bb63959d5dd0eb2391d410ba2087cdf69657b
Abigail Hahn <ahahn@systemsbiology.org>
2016-06-02 11:55:17 -0700

    Updated the picard script

--- lib/examples/container_scripts/picard.sh
+++ lib/examples/container_scripts/picard.sh
@@ -13,6 +13,6 @@ if [[ ! -z ${OUTPUT_PREFIX+x} ]]; then
 	OUTPUT_PREFIX="${OUTPUT_PREFIX}-"
 fi
 
-java -jar /usr/picard/picard.jar CollectMultipleMetrics VALIDATION_STRINGENCY=LENIENT ASSUME_SORTED=true INPUT=$INPUT_FILENAME OUTPUT="${OUTPUT_PREFIX}${INPUT_FILENAME}.multiple_metrics" PROGRAM=CollectAlignmentSummaryMetrics PROGRAM=CollectInsertSizeMetrics PROGRAM=CollectQualityYieldMetrics PROGRAM=QualityScoreDistribution REFERENCE_SEQUENCE=$REFERENCE_NAME
+java -jar /usr/picard/picard.jar CollectMultipleMetrics VALIDATION_STRINGENCY=LENIENT ASSUME_SORTED=true INPUT=$INPUT_FILENAME OUTPUT="${OUTPUT_PREFIX}${INPUT_FILENAME}.multiple_metrics" PROGRAM=CollectAlignmentSummaryMetrics PROGRAM=CollectInsertSizeMetrics PROGRAM=CollectQualityYieldMetrics PROGRAM=QualityScoreDistribution
 		
 java -jar /usr/picard/picard.jar BamIndexStats VALIDATION_STRINGENCY=LENIENT INPUT=$INPUT_FILENAME > "${OUTPUT_PREFIX}${INPUT_FILENAME}.bamIndexStats.tsv"
