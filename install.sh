33cbc8a892aaa154a027c3bdcf57c49e9415a86c
Abigail Hahn <ahahn@systemsbiology.org>
2016-06-14 16:05:31 -0700

    Getting ready to test v2

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@
 
 cp -R ../ISB-CGC-pipelines /usr/local/ISB-CGC-pipelines
 ln -s /usr/local/ISB-CGC-pipelines/lib/isb-cgc-pipelines /usr/bin/isb-cgc-pipelines
-ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineRunningJobs /usr/bin/pipelineRunningJobs
-ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineWaitingJobs /usr/bin/pipelineWaitingJobs
-ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelinePreemptedJobs /usr/bin/pipelinePreemptedJobs
-ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineDependentJobs /usr/bin/pipelineDependentJobs
-ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineMonitor /usr/bin/pipelineMonitor
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/pipelineJobScheduler /usr/bin/pipelineJobScheduler
+ln -s /usr/local/ISB-CGC-pipelines/lib/scheduler/receivePipelineVmLogs /usr/bin/receivePipelineVmLogs
 ln -s /usr/local/ISB-CGC-pipelines/lib/examples/utility_scripts/calculateDiskSize /usr/bin/calculateDiskSize
 ln -s /usr/local/ISB-CGC-pipelines/lib/examples/utility_scripts/constructCghubFilePaths /usr/bin/constructCghubFilePaths
 ln -s /usr/local/ISB-CGC-pipelines/lib/examples/utility_scripts/getChecksum /usr/bin/getChecksum
