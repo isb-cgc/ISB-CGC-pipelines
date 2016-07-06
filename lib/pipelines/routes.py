34ec99c8fe4a70c0567cc8022e2074653c94e0ff
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2016-07-05 19:48:52 -0700

    Continued with the service implementation

---
+++ lib/pipelines/routes.py
@@ -0,0 +1,11 @@
+JOBS_LIST_CREATE_EXP = r'/jobs$'
+JOB_DESCRIBE_EXP = r'/jobs/(\d+)$'
+JOB_EDIT_EXP = r'/jobs/(\d+)/edit$'
+JOB_CANCEL_EXP = r'/jobs/(\d+)/cancel$'
+JOB_RESTART_EXP = r'/jobs/(\d+)/restart$'
+CONFIG_VIEW_INIT_EXP = r'/config$'
+CONFIG_EDIT_EXP = r'/config/edit$'
+DATADISKS_LIST_CREATE_EXP = r'/datadisks$'
+DATADISK_DESCRIBE_DELETE_EXP = r'/datadisks/([a-z]*([0-9]*|[a-z]*|[A-Z-]*))*$'  # TODO: check that expression is correct
+LOG_STDOUT_EXP = r'/log/(\d+)/stdout$'
+LOG_STDERR_EXP = r'/log/(\d+)/stderr$'
