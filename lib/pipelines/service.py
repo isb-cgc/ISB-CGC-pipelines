4d9f780283e785f23c64f561a88c488bed4ff1c5
Abigail Hahn <ahahn@systemsbiology.org>
2016-09-13 16:06:03 +0000

    Fixed a typo

--- lib/pipelines/service.py
+++ lib/pipelines/service.py
@@ -84,7 +84,7 @@ class DataDisk(object):
 				if result['status'] == 'DONE':
 					break
 
-		return "projects/{p}/zones/{z}/disks/{d}".format(p=config.project_id, zones=zone, d=name)
+		return "projects/{p}/zones/{z}/disks/{d}".format(p=config.project_id, z=zone, d=name)
 
 	@staticmethod
 	def delete(config, disk_name=None, disk_zone=None):  # TODO: implement
